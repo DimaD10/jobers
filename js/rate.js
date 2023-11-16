02d7f3cae34899b07ce2a991b7f1d7aaa01027f1
DimaD10 <94060040+DimaD10@users.noreply.github.com>
2023-11-16 20:46:37 +0100

    general commit

---
+++ js/rate.js
@@ -0,0 +1,16 @@
+const rates = document.querySelectorAll('.review-rate');
+
+window.addEventListener('load', () => {
+    if (rates.length > 0) {
+        rates.forEach(rate => {
+            let mark = parseFloat(rate.getAttribute('data-rate'))
+            let width = parseFloat(rate.offsetWidth)
+
+            rate.querySelector(".review-rate__stars-rate").style.width = `${
+              (width / 5) * mark - 2 - 0.6 * mark
+            }%`;
+
+            console.log(width, mark);
+        });
+    }
+})
