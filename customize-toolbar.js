63f24bc9bdb8cce881e54e045ff7b726c29c7186
DimaD10 <94060040+DimaD10@users.noreply.github.com>
2023-12-01 07:31:43 +0100

    final

--- customize-toolbar.js
+++ customize-toolbar.js
@@ -1,20 +1,28 @@
 tinymce.init({
   selector: ".post-textarea__textarea",
   plugins:
-    "tinycomments anchor autolink charmap codesample emoticons image lists media searchreplace visualblocks wordcount checklist mediaembed casechange export formatpainter pageembed permanentpen footnotes advtemplate advtable advcode editimage tableofcontents mergetags powerpaste tinymcespellchecker autocorrect a11ychecker typography inlinecss",
+    "tinycomments anchor autolink autoresize charmap codesample emoticons image lists media searchreplace visualblocks wordcount checklist mediaembed casechange export formatpainter pageembed permanentpen footnotes advtemplate advtable advcode editimage tableofcontents mergetags powerpaste tinymcespellchecker autocorrect a11ychecker typography inlinecss",
   toolbar: "bold italic bullist",
   tinycomments_mode: "embedded",
+  autoresize_min_height: 100,
+  autoresize_max_height: 50000,
+  autoresize_bottom_margin: 20,
   setup: function (editor) {
-    editor.on("focus", e => {
+    editor.on("focus", (e) => {
       e.target.contentAreaContainer
         .closest(".post-textarea")
         .classList.add("focus");
     });
 
-    editor.on("blur", e => {
+    editor.on("blur", (e) => {
       e.target.contentAreaContainer
         .closest(".post-textarea")
         .classList.remove("focus");
     });
   },
+  content_css: ["styles/textarea.css"],
+  content_css_cors: true,
+  body_class: "editor",
+  content_style:
+    "  body, .mce-content-body[data-mce-placeholder]:not(.mce-visualblocks)::before { font-size: 16px !important; line-height: 24px !important; color: #9999a6 !important; font-family: 'Proxima Nova';} p, ul, li { color: #636d77; !important; margin: 0 !important;} body {font-family: Proxima Nova !important; } ul {padding-inline-start: 24px !important;}",
 });
